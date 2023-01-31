68d9f50e5e65153d724b78d5bf99d1bea35090ba
Luiz Daniel Lima <luizdml@gmail.com>
2023-01-31 16:11:28 -0500

    feat: added styled components to project

---
+++ .history/src/App.styled_20230131160323.ts
@@ -0,0 +1,8 @@
+export const StepCheckMark = styled.div`
+  display: ${(props: any) => (props.isFinished ? 'block' : 'none')};
+  color: ${({ theme }) => theme.colors.neutral[400]};
+
+  &:hover > ${StepLabel} {
+    color: ${({ theme }) => theme.colors.neutral[900]};
+  }
+`;
