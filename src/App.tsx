0e4589514139db8efa78170cca63948d9cd8e840
Luiz Daniel Lima <luizdml@gmail.com>
2023-02-10 10:05:59 -0500

    feat: testing GA by using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
-import ReactGA from 'react-ga';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
 import Home from './containers/Home';
 import { websiteTheme } from './theme/theme';
 
-ReactGA.initialize('G-JFNN62XGMC');
-ReactGA.pageview(window.location.pathname);
-
 function App() {
   return (
     <ThemeProvider theme={websiteTheme}>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </ThemeProvider>
   );
 }
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+
+import ReactGA from 'react-ga';
+
 import AboutMe from '../../components/AboutMe';
 import Footer from '../../components/Footer';
 import MainBanner from '../../components/MainBanner';
 import Portfolio from '../../components/Portfolio';
 import WhatIDo from '../../components/WhatIDo';
+
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+});
 
 const Home = () => {
   return (
